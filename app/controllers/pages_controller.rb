6566f4abb07bb023bf828d24f4cded46ba3c6f95
ThomasDebek <tom.debek@gmail.com>
2026-01-09 23:25:01 +0100

    Add Dark Mode to App

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def toggle_theme
+    cookies[:theme] =
+      cookies[:theme] == "dark" ? "light" : "dark"
+
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
 
+  post "theme/toggle", to: "pages#toggle_theme", as: :toggle_theme
 end
